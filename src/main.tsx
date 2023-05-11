a6ecd8ffa95941c15d1c6cebc9d7e82ec89e68e4
Bruno Kauã <brunokauaalvesdecarvalho@gmail.com>
2023-05-11 16:32:56 -0300

    Introdução ativade oque vc gosta de fazer

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+import Hobby from './paginas/home/Hobby';
+
+
+
+ReactDOM.render(
+
+  <React.StrictMode>
+
+    <Hobby />
+
+  </React.StrictMode>,
+
+
+  document.getElementById('root') as HTMLElement
+);
---
+++ src/paginas/home/Hobby.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import './Hobby.css'
+
+
+function Hobby() {
+
+    return (
+        <>
+            <nav>
+                <div>
+
+                    <span className='titulo'>Bruno</span>
+                </div>
+
+            </nav>
+            <main>
+                <h1 id='recordes'>RECORDES: </h1>
+                <section id='sobre'>
+                    <div className='texto_sobre'>
+                        <p>
+                            Olá meu nome e Bruno e tenho 18 anos eu amo treinar e com apenas 12 meses de treino pego 60kg no supino
+                            420kg no legpress 200kg no hackleg e atualmente estou em cutting
+                        </p>
+
+                    </div>
+
+                </section>
+            </main>
+
+
+            <img className='img' src="https://th.bing.com/th/id/OIP.JnZNQnDtXsgXncXEAh-J-gHaG7?pid=ImgDet&rs=1" alt="" />
+
+        </>
+
+
+    )
+
+
+
+}
+export default Hobby;
