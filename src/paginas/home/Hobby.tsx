a9fde92b628bc1ea59ac901f557b8c60e798e29e
Bruno Kauã <brunokauaalvesdecarvalho@gmail.com>
2023-05-11 22:10:54 -0300

    Colocando video da transformação

--- src/paginas/home/Hobby.tsx
+++ src/paginas/home/Hobby.tsx
@@ -14,21 +14,29 @@ function Hobby() {
 
             </nav>
             <main>
-                <h1 id='recordes'>RECORDES: </h1>
+                <h1 id='recordes'>HOBBY: </h1>
                 <section id='sobre'>
                     <div className='texto_sobre'>
                         <p>
-                            Olá meu nome e Bruno e tenho 18 anos eu amo treinar e com apenas 12 meses de treino pego 60kg no supino
-                            420kg no legpress 200kg no hackleg e atualmente estou em cutting
+                            Olá, meu nome é Bruno e tenho 18 anos. Eu amo treinar e, com apenas 12 meses de treino, consegui levantar 80kg no supino, 420kg no leg press e 200kg no hack leg. Agora, estou me preparando para entrar em uma fase de cutting para definir meu corpo.
                         </p>
+
+                        <video id='video' autoPlay muted loop controls >
+                            <source src="src\assets\VIDEO\TRANSFORMAÇAO.mp4" type="video/mp4" />
+
+                        </video>
 
                     </div>
 
                 </section>
             </main>
 
+        <footer>
 
-            <img className='img' src="https://th.bing.com/th/id/OIP.JnZNQnDtXsgXncXEAh-J-gHaG7?pid=ImgDet&rs=1" alt="" />
+
+
+        </footer>
+
 
         </>
 
--- src/paginas/home/Home.tsx
+++ src/paginas/home/Home.tsx
@@ -1,37 +1,37 @@
 function Home() {
 
 
-               //          OBS: O ARQUIVO OFICIAL ESTA NO HOBY.TSXG                                                
-    return (
-        <>
-            <nav>
-                <div>
+    //          OBS: O ARQUIVO OFICIAL ESTA NO HOBY.TSXG                                                
+return (
+<>
+ <nav>
+     <div>
 
-                    <span className='titulo'>Bruno</span>
-                </div>
+         <span className='titulo'>Bruno</span>
+     </div>
 
-            </nav>
-            <main>
-                <h1 id='recordes'>RECORDES: </h1>
-                <section id='sobre'>
-                    <div className='texto_sobre'>
-                        <p>
-                            Olá meu nome e Bruno e tenho 18 anos eu amo treinar e com apenas 12 meses de treino pego 60kg no supino
-                            420kg no legpress 200kg no hackleg e atualmente estou em cutting
-                        </p>
+ </nav>
+ <main>
+     <h1 id='recordes'>RECORDES: </h1>
+     <section id='sobre'>
+         <div className='texto_sobre'>
+             <p>
+                 Olá meu nome e Bruno e tenho 18 anos eu amo treinar e com apenas 12 meses de treino pego 60kg no supino
+                 420kg no legpress 200kg no hackleg e atualmente estou em cutting
+             </p>
 
-                    </div>
+         </div>
 
-                </section>
-            </main>
-
-
-            <img className='img' src="https://th.bing.com/th/id/OIP.JnZNQnDtXsgXncXEAh-J-gHaG7?pid=ImgDet&rs=1" alt="" />
-
-        </>
+     </section>
+ </main>
 
 
-    )
+ <img className='img' src="https://th.bing.com/th/id/OIP.JnZNQnDtXsgXncXEAh-J-gHaG7?pid=ImgDet&rs=1" alt="" />
+
+</>
+
+
+)
 
 
 
